1edf46b96a399b0da134e43fcc2a027f799166c6
Alejandro Ravasio <alejandroravasio@gmail.com>
2019-05-07 22:41:39 -0300

    Changed the way the model works and some CoreData entities settings.

---
+++ yama/Models/DatabaseManager.swift
@@ -0,0 +1,53 @@
+//
+//  DatabaseManager.swift
+//  yama
+//
+//  Created by Alejandro Ravasio on 07/05/2019.
+//  Copyright © 2019 Alejandro Ravasio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class DatabaseManager {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    func saveShows(_ shows: [Show], for category: String) {
+        shows.forEach {
+            self.saveShow($0, category: category)
+        }
+    }
+    
+    func saveShow(_ show: Show, category: String) {
+        let context = appDelegate.persistentContainer.viewContext
+        guard let entity = NSEntityDescription.entity(forEntityName: "ShowObject", in: context) else { return }
+        let newShow = NSManagedObject(entity: entity, insertInto: context)
+        newShow.setValue(category, forKey: "category")
+        newShow.setValue(show.id, forKey: "id")
+        newShow.setValue(show.title, forKey: "title")
+        newShow.setValue(show.posterPath, forKey: "posterPath")
+        newShow.setValue(show.videoPath, forKey: "videoPath")
+        newShow.setValue(show.overview, forKey: "overview")
+        newShow.setValue(show.releaseDate, forKey: "releaseDate")
+        newShow.setValue(show.backdrop, forKey: "backdrop")
+        newShow.setValue(show.rating, forKey: "rating")
+        newShow.setValue(show.voteCount, forKey: "voteCount")
+        newShow.setValue(show.originalLanguage, forKey: "originalLanguage")
+        
+        //Data encoding of the [int]; genres.
+        let genresData = try! NSKeyedArchiver.archivedData(withRootObject: show.genres, requiringSecureCoding: false)
+        newShow.setValue(genresData, forKey: "genres")
+
+        //Data encoding of the [string]; country of origin/
+        let countryOfOriginData = try! NSKeyedArchiver.archivedData(withRootObject: show.countryOfOrigin, requiringSecureCoding: false)
+        newShow.setValue(countryOfOriginData, forKey: "countryOfOrigin")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save")
+        }
+    }
+}
--- yama/Models/Show.swift
+++ yama/Models/Show.swift
@@ -24,75 +24,37 @@ struct Show: Codable {
     
     // Init created for the specific purpose of passing raw CoreData to it and map it to an actual in-memory object
     // I can more easily use across the app.
-    init(data: AnyObject) {
+    init?(data: AnyObject) {
         print(data)
-        id = data.value(forKey: "id") as? Int
-        
-        if let path = data.value(forKey: "posterPath") as? String {
-            self.posterPath = path
-        } else {
-            self.posterPath = nil
+
+        guard
+            let id = data.value(forKey: "id") as? Int,
+            let posterPath = data.value(forKey: "posterPath") as? String,
+            let videoPath = data.value(forKey: "videoPath") as? String,
+            let backdrop = data.value(forKey: "backdrop") as? String,
+            let title = data.value(forKey: "title") as? String,
+            let release = data.value(forKey: "releaseDate") as? String,
+            let rating = data.value(forKey: "rating") as? Double,
+            let count = data.value(forKey: "voteCount") as? Int,
+            let overview = data.value(forKey: "overview") as? String,
+            let genres = data.value(forKey: "genres") as? [Int],
+            let origin = data.value(forKey: "countryOfOrigin") as? [String],
+            let language = data.value(forKey: "originalLanguage") as? String else {
+                return nil
         }
         
-        if let path = data.value(forKey: "videoPath") as? String {
-            self.videoPath = path
-        } else {
-            self.videoPath = nil
-        }
-        
-        if let backdrop = data.value(forKey: "backdrop") as? String {
-            self.backdrop = backdrop
-        } else {
-            self.backdrop = ""
-        }
-        
-        if let title = data.value(forKey: "title") as? String {
-            self.title = title
-        } else {
-            self.title = ""
-        }
-        
-        if let release = data.value(forKey: "releaseDate") as? String {
-            self.releaseDate = release
-        } else {
-            self.releaseDate = ""
-        }
-        
-        if let rating = data.value(forKey: "rating") as? Double {
-            self.rating = rating
-        } else {
-            self.rating = 0
-        }
-        
-        if let count = data.value(forKey: "voteCount") as? Int {
-            self.voteCount = count
-        } else {
-            self.voteCount = 0
-        }
-        
-        if let overview = data.value(forKey: "overview") as? String {
-            self.overview = overview
-        } else {
-            self.overview = ""
-        }
-        
-        if let genres = data.value(forKey: "genres") as? [Int] {
-            self.genres = genres
-        } else {
-            self.genres = []
-        }
-        
-        if let origin = data.value(forKey: "countryOfOrigin") as? [String] {
-            self.countryOfOrigin = origin
-        } else {
-            self.countryOfOrigin = []
-        }
-        
-        if let language = data.value(forKey: "originalLanguage") as? String {
-            self.originalLanguage = language
-        } else {
-            self.originalLanguage = ""
-        }
+        self.id = id
+        self.posterPath = posterPath
+        self.videoPath = videoPath
+        self.backdrop = backdrop
+        self.title = title
+        self.releaseDate = release
+        self.rating = rating
+        self.voteCount = count
+        self.overview = overview
+        self.genres = genres
+        self.countryOfOrigin = origin
+        self.originalLanguage = language
     }
     
     private enum CodingKeys: String, CodingKey {
